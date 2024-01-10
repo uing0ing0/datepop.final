234793df2d700976bed1d367e9ff9ec0deff01a1
Kimcheolhui <cjfgml0306@naver.com>
2024-01-10 17:34:18 +0900

    Add address, lat-lon columns

--- eagle_eye/crawling/blueribbon_crawling.py
+++ eagle_eye/crawling/blueribbon_crawling.py
@@ -2,15 +2,17 @@ import pandas as pd
 import requests
 from requests.exceptions import HTTPError, ConnectionError
 
+
 class BlueRibbonCrawler:
     def __init__(self, location):
         self.location = location
         self.current_page = 0
         self.max_page = 0
-        self.data = pd.DataFrame(columns=["name", "ribbonType"])
+        self.data = pd.DataFrame(
+            columns=["name", "ribbonType", "latitude", "longitude", "address"])
 
     def crawling(self):
-        print("Blue Ribbon Survey crawling start!")
+        print("Blue Ribbon Survey crawling start......")
         # initialization
         try:
             print("Crawling page 0......")
@@ -27,12 +29,35 @@ class BlueRibbonCrawler:
 
     def crawling_onepage(self):
         try:
-            response = requests.get(f"https://www.bluer.co.kr/api/v1/restaurants?page={self.current_page}&query={self.location}")
+            response = requests.get(
+                f"https://www.bluer.co.kr/api/v1/restaurants?page={self.current_page}&query={self.location}")
+
             if self.max_page == 0:
                 self.max_page = response.json()["page"]["totalPages"]
             items = response.json()['_embedded']['restaurants']
-            data = [{"name": item["headerInfo"]["nameKR"], "ribbonType": item["headerInfo"]["ribbonType"]} for item in items]
+            data = []
+            for item in items:
+                name = item["headerInfo"]["nameKR"]
+                ribbonType = item["headerInfo"]["ribbonType"]
+                latitude = item["gps"]["latitude"]
+                longitude = item["gps"]["longitude"]
+
+                print(latitude)
+
+                roadAddrPart1 = item["juso"]["roadAddrPart1"]
+                detailAddress = item["juso"]["detailAddress"]
+
+                # None 확인 및 대체
+                roadAddrPart1 = "" if roadAddrPart1 is None else roadAddrPart1
+                detailAddress = "" if detailAddress is None else detailAddress
+
+                address = roadAddrPart1 + " " + detailAddress
+
+                data.append({"name": name, "ribbonType": ribbonType, "latitude": latitude,
+                             "longitude": longitude, "address": address})
+
             new_data = pd.DataFrame(data)
+
             self.data = pd.concat([self.data, new_data], ignore_index=True)
             self.current_page += 1
         except (HTTPError, ConnectionError) as e:
@@ -42,13 +67,8 @@ class BlueRibbonCrawler:
             print(f"An error occurred: {e}")
 
 
-# if __name__ == "__main__":
-#     location_input = input()
-#     ribbon_crawler = BlueRibbonCrawler(location_input)
-#     ribbon_crawler.crawling()
-#     if "목로" in ribbon_crawler.data['name'].values:
-#         print("목로 리본 등급: ", ribbon_crawler.data.loc[ribbon_crawler.data["name"] == "목로", "ribbonType"].iloc[0])
-#     if "가나다" in ribbon_crawler.data['name'].values:
-#         print("가나다 리본 등급: ", ribbon_crawler.data.loc[ribbon_crawler.data["name"] == "가나다", "ribbonType"].iloc[0])
-#     if "어썸로즈" in ribbon_crawler.data['name'].values:
-#         print("어썸로즈 리본 등급: ", ribbon_crawler.data.loc[ribbon_crawler.data["name"] == "어썸로즈", "ribbonType"].iloc[0])
+if __name__ == "__main__":
+    # location_input = input()
+    location_input = "가로수길"
+    ribbon_crawler = BlueRibbonCrawler(location_input)
+    ribbon_crawler.crawling()
