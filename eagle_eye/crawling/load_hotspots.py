7d6b62b8e2a9cc3a6047a8a00990007c0fe5e3fb
Kimcheolhui <cjfgml0306@naver.com>
2024-01-11 17:49:05 +0900

    Change loading hotspots func to be more general

--- eagle_eye/crawling/load_hotspots.py
+++ eagle_eye/crawling/load_hotspots.py
@@ -3,12 +3,12 @@ import os
 from shapely.geometry import Polygon
 
 
-def load_seoul_hotspots():
-    seoul_hotspots = []
+def load_hotspots(file_name):
+    hotspots = []
 
     # 스크립트 파일이 위치한 디렉토리를 기준으로 상대 경로 구하기
     directory = os.path.dirname(os.path.abspath(__file__))
-    csv_file_path = os.path.join(directory, 'data', 'seoul_hotspots.csv')
+    csv_file_path = os.path.join(directory, 'data', file_name)
 
     # CSV 파일 읽기
     with open(csv_file_path, 'r', encoding='utf-8') as file:
@@ -19,7 +19,7 @@ def load_seoul_hotspots():
             location = row[0]
             polygon_coords = [(float(x), float(y)) for x, y in (
                 point.split(',') for point in row[1].split(';'))]
-            seoul_hotspots.append(
+            hotspots.append(
                 {"location": location, "polygon_area": Polygon(polygon_coords)})
 
-    return seoul_hotspots
+    return hotspots
