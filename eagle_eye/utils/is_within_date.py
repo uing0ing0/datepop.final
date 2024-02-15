73fb6426e23d811771d5514fb7a3c7da134ecdcd
Kimcheolhui <cjfgml0306@naver.com>
2024-02-15 10:10:16 +0900

    fix: remove locale code, modify dealing date func

--- eagle_eye/utils/is_within_date.py
+++ eagle_eye/utils/is_within_date.py
@@ -2,22 +2,24 @@ from datetime import datetime, timedelta
 
 
 def is_within_one_month(date_text):
+    # 요일 정보를 제외하고 날짜만 추출
+    date_text_formatted = ' '.join(date_text.split(' ')[:3])
+    date = datetime.strptime(date_text_formatted, "%Y년 %m월 %d일")
+
     # 현재 날짜
     current_date = datetime.now()
-
-    # 날짜 텍스트를 datetime 객체로 변환
-    date = datetime.strptime(date_text, "%Y년 %m월 %d일 %A")
 
     # 날짜가 한 달 이내인지 확인
     return current_date - timedelta(days=30) <= date <= current_date
 
 
 def is_within_two_weeks(date_text):
+    # 요일 정보를 제외하고 날짜만 추출
+    date_text_formatted = ' '.join(date_text.split(' ')[:3])
+    date = datetime.strptime(date_text_formatted, "%Y년 %m월 %d일")
+
     # 현재 날짜
     current_date = datetime.now()
 
-    # 날짜 텍스트를 datetime 객체로 변환
-    date = datetime.strptime(date_text, "%Y년 %m월 %d일 %A")
-
-    # 날짜가 한 달 이내인지 확인
+    # 날짜가 두 주 이내인지 확인
     return current_date - timedelta(days=14) <= date <= current_date
