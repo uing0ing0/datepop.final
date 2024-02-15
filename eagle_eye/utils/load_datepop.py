15d2e3426d322dc8547bcfab3a3c2b987f7db36f
Kimcheolhui <cjfgml0306@naver.com>
2024-02-15 15:23:22 +0900

    fix: load different datepop data depending on is_food

--- eagle_eye/utils/load_datepop.py
+++ eagle_eye/utils/load_datepop.py
@@ -3,11 +3,14 @@ import os
 from shapely.geometry import Polygon
 
 
-def load_datepop():
+def load_datepop(is_food: bool):
 
     directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     csv_directory = os.path.join(directory, 'data/datepop')
-    csv_file_path = os.path.join(csv_directory, "shop_playing.csv")
+    if is_food:
+        csv_file_path = os.path.join(csv_directory, "shop_food.csv")
+    else:
+        csv_file_path = os.path.join(csv_directory, "shop_playing.csv")
 
     df = pd.read_csv(
         csv_file_path, encoding="utf-8-sig").drop('Unnamed: 0', axis=1).reset_index(drop=True)
